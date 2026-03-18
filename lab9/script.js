634ba488ac87a71ff9d17b4a4c7c651d7ad9188b
Will Manicom <wmanicom11@gmail.com>
2026-03-18 16:36:07 -0400

    completed lab 9

---
+++ lab9/script.js
@@ -0,0 +1,26 @@
+async function fetchData() {
+    try {
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=43.05005612832071&longitude=-76.1469198697371&current=precipitation,temperature_2m,cloud_cover&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch');
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const precipitation = data.current.precipitation;
+        const temperature = data.current.temperature_2m;
+        const cloudCover = data.current.cloud_cover;
+
+        document.getElementById('precipitation').textContent = "Precipitation: " + precipitation + '"';
+        document.getElementById('temperature').textContent = "Temperature: " + temperature + '°F';
+
+        const cloudEmoji = cloudCover > 50 ? '☁️' : '☀️';
+        document.getElementsByClassName('emoji')[0].textContent = cloudEmoji;
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+fetchData();
